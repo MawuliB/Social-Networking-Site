1be13fec2e530ac38ab5653eb1788c272da90c23
Mawuli Badassou <mawulibadassou5@gmail.com>
2024-05-06 18:55:56 +0000

    push to prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     //production: false,
-    prodution: true,
+    production: true,
     //API_URL: 'http://localhost:8080',
     API_URL: 'https://sns-image-latest.onrender.com'
     };
