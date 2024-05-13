5208f658eee6255e53f457e566e446171ea837c3
Mawuli Badassou <mawulibadassou5@gmail.com>
2024-05-13 19:28:24 +0000

    push to prod

--- src/app/my-stomp.service.ts
+++ src/app/my-stomp.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import SockJS from 'sockjs-client';
 import * as Stomp from 'stompjs'
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MyStompService {
+    url = environment.API_URL
 
-  socket = new SockJS('http://localhost:8080/ws');
+  socket = new SockJS(`${this.url}/ws`);
   stompClient = Stomp.over(this.socket);
 }
