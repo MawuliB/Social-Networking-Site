1f8909dc017f04de72d2c0469adef5f8bb08b630
Mawuli <66177382+MawuliB@users.noreply.github.com>
2024-05-26 01:27:00 +0000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     // production: false,
     production: true,
     // API_URL: 'http://localhost:8080',
-    API_URL: 'http://ec2-54-146-148-29.compute-1.amazonaws.com'
+    API_URL: 'https://sns-image-latest.onrender.com'
 };
