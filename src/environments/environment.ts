e3b1002e0deab53be137f67bb0810ec30c4eadd7
Mawuli Badassou <mawulibadassou5@gmail.com>
2024-06-12 11:42:47 +0000

    Fix(Env) changed url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,7 @@ export const environment = {
     // production: false,
     production: true,
     // API_URL: 'http://localhost:8080',
-    API_URL: 'https://sns-image-latest.onrender.com'
+    // API_URL: 'https://sns-image-latest.onrender.com',
+    // API_URL: 'https://5dd7-197-159-140-9.ngrok-free.app',
+    API_URL: 'https://www.free-sns.live',
 }
