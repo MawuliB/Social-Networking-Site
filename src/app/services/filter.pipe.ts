af5c1cbd972525fa1203689511d6a2b12c4449d7
Mawuli Badassou <mawulibadassou5@gmail.com>
2024-06-14 14:25:22 +0000

    Fix(Find Contacts) Slicing the contacts to display only 10 | push to prod

--- src/app/services/filter.pipe.ts
+++ src/app/services/filter.pipe.ts
@@ -6,9 +6,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterPipe implements PipeTransform {
   transform(items: any[], searchTerm: string): any[] {
     if (!items) return [];
-    if (!searchTerm) return items;
+    if (!searchTerm) return items.slice(0, 10);
   
     searchTerm = searchTerm.toLowerCase().trim();
+
+    if (searchTerm.length === 0) {
+      return items.slice(0, 10);
+    }
   
     return items.filter(it => {
       return (it.email ? it.email.toLowerCase().includes(searchTerm) : false) || 
